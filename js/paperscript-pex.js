4694b6e75a6c175d7306ecbc6c48d9456df9acfc
NiallNiall <niall@nimac.local>
2015-05-14 18:01:02 +0100

    Broken update

---
+++ js/paperscript-pex.js
@@ -0,0 +1,5 @@
+// Create a Paper.js Path to draw a line into it:
+    var path = new Path();
+    // Give the stroke a color
+    var myCircle = new Path.Circle(new Point(100, 70), 50);
+    myCircle.fillColor = 'black';
